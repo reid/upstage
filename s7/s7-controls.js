9093cbeb99874037355f87682499869bba8baf03
Reid Burke <me@reidburke.com>
2010-09-23 15:47:37 -0400

    Parted out CSS, minor #currentSlide fix.

--- s7/s7-controls.js
+++ s7/s7-controls.js
@@ -1,22 +1,25 @@
 YUI.add("s7-controls", function (Y) {
 
-    var controls = Y.one("#controls");
+    var controls = Y.one("#nav");
 
     if (
         controls === null
     ) throw new Error("controls are missing");
 
-    Y.S7.on("start", function () {
-        controls.one("#prev").on("click", Y.S7.hire("warp", -1));
-        controls.one("#next").on("click", Y.S7.hire("warp", 1));        
-    });
-
-    // absolute position
-    Y.S7.on("navigate", function (idx) {
+    function syncCurrentSlide (idx) {
         controls.one("#currentSlide").setContent(
             idx + "/" + Y.all(".slide").size()
         );
+    }
+
+    Y.S7.on("start", function () {
+        controls.one("#prev").on("click", Y.S7.hire("warp", -1));
+        controls.one("#next").on("click", Y.S7.hire("warp", 1));        
+        syncCurrentSlide(1);
     });
+
+    // absolute position
+    Y.S7.on("navigate", syncCurrentSlide);
 
 }, "0.0.1", {
     requires : [
