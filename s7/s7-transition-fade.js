ebdfd19689538127b7a717229c4035584d8aebe1
Reid Burke <me@reidburke.com>
2010-09-26 17:07:29 -0400

    Fade transition now works with rapid keystrokes.

--- s7/s7-transition-fade.js
+++ s7/s7-transition-fade.js
@@ -1,16 +1,47 @@
 YUI.add("s7-transition-fade", function (Y) {
 
+    var running = false;
+    var queue = new Y.AsyncQueue;
+
+    function shutdown () {
+        running = false;
+        queue.stop();
+    }
+
     Y.S7.on("transition", function (ev) {
+
+        if (running) {
+            // A transition is already in progress.
+            // Run S7's default transition behavior
+            // by reverting our styles and not calling
+            // ev.preventDefault. Finally, tell the
+            // current transitions to halt.
+            Y.all(".slide").setStyles({
+                "opacity" : "1",
+                "display" : "none"
+            });
+            return shutdown();
+        }
+
         ev.preventDefault();
+        running = true;
 
         var prev = ev.details[0],
             next = ev.details[1];
-        
-        prev.transition({
-            duration : 0.2,
-            easing : "ease-out",
-            opacity : 0
-        }, function () {
+
+        queue.add(function () {
+            // run the next function after transition completes:
+            queue.pause();
+            // we use AsyncQueue just for the stop() ability.
+            prev.transition({
+                duration : 0.2,
+                easing : "ease-out",
+                opacity : 0
+            }, Y.bind(queue.run, queue));
+        });
+
+        queue.add(function () {
+            queue.pause();
             next.setStyles({
                 "opacity" : "0",
                 "display" : "block"
@@ -19,13 +50,19 @@ YUI.add("s7-transition-fade", function (Y) {
                 duration : 0.2,
                 easing : "ease-out",
                 opacity : 1
-            });
+            }, Y.bind(queue.run, queue));
         });
+
+        queue.add(shutdown);
+
+        queue.run();
+
     });
 
 }, "0.0.1", {
     requires : [
         "s7-slideshow",
-        "transition"
+        "transition",
+        "async-queue"
     ]
 });
